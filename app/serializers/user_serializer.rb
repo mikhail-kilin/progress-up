4f00aff4f3144de850e2dfdd9dd5b9d72daaa9ec
Mikhail Kilin <mikhail.kilin@flatstack.com>
2021-05-29 11:13:52 +0300

    fix

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ApplicationSerializer
-  attributes :id, :email, :full_name, :phone, :description, :avatar_url
+  attributes :id, :email, :full_name, :phone, :description, :avatar_url, :article_ids
 
   def avatar_url
     object.avatar.url
